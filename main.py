11008e86ab91450bcdc914edbe7b24bc6a472212
Kyle Rimando <riman004@csusm.edu>
2019-07-31 15:47:31 -0700

    Fortune teller

--- main.py
+++ main.py
@@ -60,7 +60,12 @@ class FortuneHandler(webapp2.RequestHandler):
         self.response.write(start_template.render())
 
     def post(self):
-        my_dict={}
+        random_fortune = get_fortune()
+        user_astrological_sign = self.request.get("user_astrological_sign")
+        my_dict={
+        "the_fortune": random_fortune,
+        "user_sign": user_astrological_sign
+        }
         end_template=jinja_current_directory.get_template("templates/fortune_results.html")
         self.response.write(end_template.render(my_dict))
 
